9efd3ca7b1641ea6905d1c71bbbdca9728cc9ca8
Jameson Trinker <jameson.trinker@gmail.com>
2016-03-17 17:03:32 -0400

    contact form

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -7,7 +7,18 @@ export default Ember.Controller.extend({
 	validEmail: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
 	validMessage: Ember.computed.gte('messageBody.length', 5),
 
-	enableSend: Ember.computed.and('validEmail', 'validMessage'),
-	disableSend: Ember.computed.not('enableSend')
+	disableSend: Ember.computed('validEmail', 'validMessage', function() {
+		if (this.get('validEmail') === false || this.get('validMessage') === false) {
+			return true;
+		}
+	}),
+
+	actions: {
+		sendMessage() {
+			this.set('responseMessage', `Thank you for contacting us! We will email you back at ${this.get('emailAddress')}`);
+			this.set('emailAddress', '');
+			this.set('messageBody', '');
+		}
+	}
 
 });
